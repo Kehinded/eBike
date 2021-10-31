f1c8df4b43091d11b443ce3c93f2015291fc8b39
kehinded <80984106+Kehinded@users.noreply.github.com>
2021-10-31 22:28:53 +0100

    Update Dot.js

--- src/fragment/Dot.js
+++ src/fragment/Dot.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from "react";
+import "../styles/Dot.css";
 
-const Dot = () => {
+const Dot = ({ top, bottom, left, right, style, dotFour, bgcolor }) => {
+  if (dotFour) {
     return (
-        <div>
-            
-        </div>
-    )
-}
+      <div
+        className={`dot-box ${style}`}
+        style={{
+          position: "absolute",
+          top: `${top}`,
+          bottom: `${bottom}`,
+          left: `${left}`,
+          right: `${right}`,
+          zIndex: "20",
+        }}
+      >
+        <div style={{ backgroundColor: `${bgcolor}` }} className="dot"></div>
+        <div style={{ backgroundColor: `${bgcolor}` }} className="dot"></div>
+        <div style={{ backgroundColor: `${bgcolor}` }} className="dot"></div>
+        <div style={{ backgroundColor: `${bgcolor}` }} className="dot"></div>
+        <div style={{ backgroundColor: `${bgcolor}` }} className="dot"></div>
+        <div style={{ backgroundColor: `${bgcolor}` }} className="dot"></div>
+        <div style={{ backgroundColor: `${bgcolor}` }} className="dot"></div>
+        <div style={{ backgroundColor: `${bgcolor}` }} className="dot"></div>
+      </div>
+    );
+  }
+  return (
+    <div
+      className={`dot-box ${style}`}
+      style={{
+        position: "absolute",
+        top: `${top}`,
+        left: `${left}`,
+        right: `${right}`,
+      }}
+    >
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+    </div>
+  );
+};
 
-export default Dot
+export default Dot;
+
