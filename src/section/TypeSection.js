78164864f1b30d85e43c48bb310d19636730068a
kehinded <80984106+Kehinded@users.noreply.github.com>
2021-10-31 22:34:57 +0100

    Update TypeSection.js

--- src/section/TypeSection.js
+++ src/section/TypeSection.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import TypeCard from "../fragment/TypeCard";
 
 const TypeSection = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="type-section" id="types">
+      <h3 className="title">other types of bikes</h3>
+      <div className="card-box">
+        <TypeCard text="hybrid bikes" />
+        <TypeCard text="fixie bikes" />
+        <TypeCard text="folding bikes" />
+        <TypeCard text="mount bikes" />
+        <TypeCard text="city bikes" />
+        <TypeCard text="lolly bikes" />
+        <TypeCard text="jeda bikes" />
+      </div>
+    </div>
+  );
+};
 
-export default TypeSection
+export default TypeSection;
+
