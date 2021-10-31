af48e7e68bba1c6bc4a1fb0529a56cac79814614
kehinded <80984106+Kehinded@users.noreply.github.com>
2021-10-31 22:26:23 +0100

    Update ProductCard.js

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -1,11 +1,23 @@
-import React from "react"
+import React from "react";
+import Button from "../fragment/Button";
+import "../styles/Product.css";
 
-const ProductCard = () => {
-    return(
-        <div>
-
+const ProductCard = ({ title, img, tranvalue }) => {
+  return (
+    <div className="card" style={{ transform: `translateX(${tranvalue}%)` }}>
+      {title && <h3 className="title">{title || "title"}</h3>}
+      <figure className="img-box">
+        <img src={img} alt="" className="img" />
+      </figure>
+      <div className="price-box">
+        <div className="price-value-box">
+          <p className="text">price</p>
+          <p className="value">$750.00</p>
         </div>
-    )
-}
+        <Button text="order" />
+      </div>
+    </div>
+  );
+};
 
-export default ProductCard; 
+export default ProductCard;
