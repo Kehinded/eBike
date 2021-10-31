5b347ec3ea333072436671fb34833b5128ad3956
kehinded <80984106+Kehinded@users.noreply.github.com>
2021-10-31 22:30:15 +0100

    Update TypeCard.js

--- src/fragment/TypeCard.js
+++ src/fragment/TypeCard.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "../styles/Type.css";
+import ImgDoc from "../asset/typeimg.jpg";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
-const TypeCard = () => {
-  return <div></div>;
+const TypeCard = ({ img, text }) => {
+  useEffect(() => {
+    Aos.init({
+      duration: 500,
+      offset: 200,
+    });
+  }, []);
+  return (
+    <div className="card-wrapper">
+      <div className="card" data-aos="fade-in">
+        <figure className="img-box">
+          <img src={ImgDoc} alt="" className="img" />
+        </figure>
+      </div>
+      <p data-aos="fade-down" className="text">
+        {text || "text"}
+      </p>
+    </div>
+  );
 };
 
 export default TypeCard;
