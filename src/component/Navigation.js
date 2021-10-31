ff4f9126e6b522fc8078cc0f016d8029ab4bb2e5
kehinded <80984106+Kehinded@users.noreply.github.com>
2021-10-31 22:25:15 +0100

    Update Navigation.js

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,37 +1,43 @@
-import React from "react"
-import { Link, useLocation } from "react-router-dom";
+import React from "react";
+import { Link } from "react-scroll";
 
 const Navigation = () => {
-    const location = useLocation()
-    return (
-      <ul className="nav-list">
-        <li className="nav-item">
-          <Link to="/products" className="nav-link active">
-            product
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/types" className="nav-link">
-            bike type
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/about" className="nav-link">
-            about us
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/testimonies" className="nav-link">
-            testimonials
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/contact" className="nav-link">
-            contact
-          </Link>
-        </li>
-      </ul>
-    );
-}
+  // const location = useLocation();
+  return (
+    <ul className="nav-list">
+      <li className="nav-item">
+        <Link
+          activeClass="active"
+          to="products"
+          spy={true}
+          smooth={true}
+          className="nav-link active"
+        >
+          product
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link to="types" spy={true} smooth={true} className="nav-link">
+          bike type
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link to="about" spy={true} smooth={true} className="nav-link">
+          about us
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link to="testimonies" spy={true} smooth={true} className="nav-link">
+          testimonials
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link to="contact" spy={true} smooth={true} className="nav-link">
+          contact
+        </Link>
+      </li>
+    </ul>
+  );
+};
 
 export default Navigation;
