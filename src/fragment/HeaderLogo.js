90219bf1357972587f29a1cb0a092ef0847a5ab8
kehinded <80984106+Kehinded@users.noreply.github.com>
2021-10-31 22:29:38 +0100

    Update HeaderLogo.js

--- src/fragment/HeaderLogo.js
+++ src/fragment/HeaderLogo.js
@@ -1,16 +1,16 @@
-import React from "react"
-import "../styles/Header.css"
+import React from "react";
+import "../styles/Header.css";
 
-const HeaderLogo = ({roundColor}) => {
-    return (
-      <div className="logo-box">
-        <div className={`round-text ${roundColor}`}>
-          {" "}
-          <span>a</span>{" "}
-        </div>
-        <p className="text">eBike</p>
+const HeaderLogo = ({ roundColor }) => {
+  return (
+    <div className="logo-box">
+      <div className={`round-text ${roundColor}`}>
+        {" "}
+        <span>a</span>{" "}
       </div>
-    );
-}
+      <p className="text">eBike</p>
+    </div>
+  );
+};
 
 export default HeaderLogo;
